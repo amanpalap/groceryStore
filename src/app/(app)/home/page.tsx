260086171f8037b605e8b28aa1a7d3c53dd71e55
amanpalap <pal90589500@gmail.com>
2024-07-28 14:14:36 +0530

    update 3 files and create 5 files

---
+++ src/app/(app)/home/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { AppleCardsCarouselDemo } from '@/components/carousal'
+
+export default function page() {
+    return (
+        <div>
+            <AppleCardsCarouselDemo />
+        </div>
+    )
+}
+
