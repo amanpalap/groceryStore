216b48b12572881922f94caa1dcd6e384f5f5658
amanpalap <pal90589500@gmail.com>
2024-08-02 04:00:59 +0530

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
     domains: ["images.unsplash.com", "assets.aceternity.com"],
   },
   eslint: { ignoreDuringBuilds: true },
+  async redirects() {
+    return [
+      {
+        source: "/new-home",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
