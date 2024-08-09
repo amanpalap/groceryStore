2986fac66f74f78fb5096d82f6f0ac20af5aa7bd
amanpalap <pal90589500@gmail.com>
2024-08-09 16:40:41 +0530

    update page.tsx

--- src/app/(app)/user/cart/page.tsx
+++ src/app/(app)/user/cart/page.tsx
@@ -1,107 +1,111 @@
 'use client'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { remove, setAmount } from '@/lib/store/features/cart/cartSlice'
-import { useAppSelector, useAppDispatch } from '@/lib/store/hooks/hooks'
-import { useForm } from 'react-hook-form'
-import { cartSchema } from '@/schemas/cartSchemas'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { remove, setAmount } from '@/lib/store/features/cart/cartSlice';
+import { useAppSelector, useAppDispatch } from '@/lib/store/hooks/hooks';
+import { useForm } from 'react-hook-form';
+import { cartSchema } from '@/schemas/cartSchemas';
 import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod'
-import Image from 'next/image'
+import { zodResolver } from '@hookform/resolvers/zod';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { ArrowDown, ArrowUp } from 'lucide-react';
 
 const Page = () => {
-    const cartItems = useAppSelector((state) => state.cart) // Assuming cart is an object with an 'items' array
-    const dispatch = useAppDispatch()
+    const cartItems = useAppSelector((state) => state.cart);
+    const dispatch = useAppDispatch();
+    const [isMounted, setIsMounted] = useState(false);
 
     const form = useForm<z.infer<typeof cartSchema>>({
         resolver: zodResolver(cartSchema),
         defaultValues: {
-            weight: 0
-        }
-    })
+            weight: 0,
+        },
+    });
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
 
     const onSubmit = () => {
         // Handle form submission
-    }
+    };
 
     const amountHandler = (id: number, amount: number) => {
-        dispatch(setAmount({ id, amount }))
-    }
+        dispatch(setAmount({ id, amount }));
+    };
 
     const handleRemove = (productId: number) => {
-        dispatch(remove(productId))
-    }
+        dispatch(remove(productId));
+    };
 
     const amountIncrementor = (id: number, amount: number) => {
         if (amount < 0.5 && amount >= 0) {
-            amountHandler(id, amount + 0.1)
+            amountHandler(id, amount + 0.1);
         } else if (amount >= 0.5 && amount < 1) {
-            amountHandler(id, amount + 0.25)
+            amountHandler(id, amount + 0.25);
         } else if (amount >= 1 && amount < 10) {
-            amountHandler(id, amount + 0.5)
+            amountHandler(id, amount + 0.5);
         }
-    }
+    };
 
     const amountDecrementor = (id: number, amount: number) => {
         if (amount <= 0.5 && amount > 0) {
-            amountHandler(id, amount - 0.1)
+            amountHandler(id, amount - 0.1);
         } else if (amount > 0.5 && amount <= 1) {
-            amountHandler(id, amount - 0.25)
+            amountHandler(id, amount - 0.25);
         } else if (amount > 1) {
-            amountHandler(id, amount - 0.5)
+            amountHandler(id, amount - 0.5);
         }
-    }
+    };
 
     return (
-        <div className='w-full p-8'>
+        <div className='w-full grid p-8'>
             <h1 className='w-full font-extrabold text-5xl mb-8'>Your Cart</h1>
             {cartItems.map((item, idx) => (
-                <div key={idx} className='w-full border-2 flex flex-wrap' >
-                    <div className="w-full px-8 flex">
-                        <Image className='' src={item.image} alt={''} width={"250"} height={"150"} />
+                <div key={item.id} className='flex w-[45%] flex-wrap border-2'>
+                    <div className="items-between border-2 space-x-10 flex">
+                        <Image src={item.image} className="rounded-l-xl w-52 h-32 aspect-square" alt={`${item.names[0]} image`} width={"100"} height={"100"} />
 
                         <div className='flex flex-wrap'>
-                            <h3 className='text-2xl h-fit border-2'>
+                            <h3 className='text-2xl h-fit'>
                                 {item.names[0]} ({item.names[1]})
                             </h3>
 
                             <Form {...form}>
-                                <form onSubmit={form.handleSubmit(onSubmit)} className="justify-between border-2 flex flex-wrap">
-
+                                <form onSubmit={form.handleSubmit(onSubmit)} className="justify-between w-full border-2 flex flex-wrap">
                                     <FormField
                                         name="weight"
                                         control={form.control}
                                         render={({ field, fieldState: { error } }) => (
-                                            <FormItem>
-                                                <FormLabel className='border-2'>Weight</FormLabel>
+                                            <FormItem className='flex flex-wrap h-fit'>
+                                                <FormLabel>Weight</FormLabel>
                                                 <FormControl className='w-full'>
                                                     <div className='flex justify-between items-center'>
                                                         <Input
                                                             className='w-20 input-no-spinner'
                                                             {...field}
                                                             type="number"
-                                                            value={item.amount || ""}
+                                                            value={item.amount.toFixed(2) || ""}
                                                             placeholder='0'
                                                             onChange={(e) => amountHandler(item.id, Number(e.target.value))}
                                                         />
                                                         <span>/kg</span>
                                                         <div className='border-2 flex w-10 rounded-lg overflow-hidden flex-wrap'>
-                                                            <button
+                                                            <ArrowUp type="button"
                                                                 onClick={() => amountIncrementor(item.id, item.amount)}
-                                                                className='bg-white text-black h-5 w-full'
-                                                            >
-                                                                ^
-                                                            </button>
-                                                            <button
+                                                                className='bg-white text-black flex jsutify-center items-center h-5 w-full' />
+                                                            <ArrowDown type="button"
                                                                 onClick={() => amountDecrementor(item.id, item.amount)}
-                                                                className='bg-white text-black h-5 w-full'
-                                                            >
-                                                                v
-                                                            </button>
+                                                                className='bg-white text-black flex jsutify-center items-center h-5 w-full' />
                                                         </div>
                                                     </div>
                                                 </FormControl>
-                                                {error?.message && <FormMessage className="text-xs"> {error.message}</FormMessage>}
+                                                {error?.message && <FormMessage className="text-xs">{error.message}</FormMessage>}
                                             </FormItem>
                                         )}
                                     />
@@ -109,7 +113,8 @@ const Page = () => {
                             </Form>
 
                             <button
-                                className="bg-red-500"
+                                type="button"
+                                className="bg-red-500 px-5 rounded-xl hover:text-black"
                                 onClick={() => handleRemove(item.id)}
                             >
                                 Remove
@@ -119,7 +124,7 @@ const Page = () => {
                 </div>
             ))}
         </div>
-    )
+    );
 }
 
-export default Page
+export default Page;
