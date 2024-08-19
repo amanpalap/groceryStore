aa41e424286cb48e0464c50d5e2bc72a3d7c601d
amanpalap <pal90589500@gmail.com>
2024-08-20 02:17:13 +0530

    update page.tsx and route.ts

--- src/app/(app)/(admin)/orders/page.tsx
+++ src/app/(app)/(admin)/orders/page.tsx
@@ -29,7 +29,7 @@ const Page = () => {
     useEffect(() => {
         const handleData = async () => {
             try {
-                const response = await axios.post('/api/orders')
+                const response = await axios.get('/api/orders')
                 setData(response.data.data)
             } catch (error) {
                 console.log("error getting orders", error)
--- src/app/api/(admin)/orders/route.ts
+++ src/app/api/(admin)/orders/route.ts
@@ -2,18 +2,18 @@ import dbConnect from "@/lib/dbConnect";
 import OrdersModel from "@/models/cart";
 import { NextResponse } from "next/server";
 
-export async function POST(response: NextResponse) {
+export async function GET() {
     await dbConnect()
     try {
         const data = await OrdersModel.find()
 
-        if (!data) {
+        if (!data || data.length === 0) {
             return NextResponse.json(
                 {
                     success: false,
                     message: "data not found"
                 },
-                { status: 400 }
+                { status: 404 }
             )
         }
         console.log(data)
